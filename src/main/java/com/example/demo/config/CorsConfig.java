25bf53682e35b5034e8f3b6b2e32dd43ffa54893
Aliyan Ahmed Naqvi <109412829+Aaaly123@users.noreply.github.com>
2025-11-13 14:48:10 +0500

    Fix of Cors adding origin for deployed frontend on Vercel

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://whisp-frontend.vercel.app")
+                        .allowedOrigins("http://localhost:5173", "https://whisp-sigma.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
